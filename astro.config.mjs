8f1711bf4e7ca139c788f2d9f8cb6e09866746b9
Andy Shinn <andys@andyshinn.as>
2025-07-14 20:41:03 -0500

    fix webmanifest location

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,18 @@ import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://austinmesh.org',
   integrations: [
     icon(),
     webmanifest({
       name: 'Austin Mesh',
       icon: 'src/images/android-chrome-512x512.png',
       short_name: 'Austin Mesh',
-      description: 'Here is your app description',
+      description: 'Austin Mesh is a community of mesh networking enthusiasts in Austin, Texas. We build and maintain a free, open-source, decentralized mesh network for public and private communication.',
       start_url: '/',
       theme_color: '#67EA94',
       background_color: '#222222',
-      display: 'standalone',
+      display: 'browser',
     }),
   ],
   vite: {
