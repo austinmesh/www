e48f0a03a081ba8ce43c78a2999b41626a6b1284
Andy Shinn <andys@andyshinn.as>
2025-07-14 19:37:21 -0500

    automatic webmanifest stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    webmanifest({
+      name: 'Austin Mesh',
+      icon: 'src/images/android-chrome-512x512.png',
+      short_name: 'Austin Mesh',
+      description: 'Here is your app description',
+      start_url: '/',
+      theme_color: '#67EA94',
+      background_color: '#222222',
+      display: 'standalone',
+    }),
+  ],
   vite: {
     server:{
       allowedHosts: ['localhost', '95d09d8539b0.ngrok.app'],
